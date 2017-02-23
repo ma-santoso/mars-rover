52a17eaddc78c7b7375e8ff3aee19345ea69eb82
ma-santoso <marsa.agung.s@mail.ugm.ac.id>
2017-02-24 00:11:58 +0700

    Refactor

--- mars_rover.py
+++ mars_rover.py
@@ -1,23 +1,38 @@
 import praw
+import time
 
 bot = "mars-rover"
-sub = "indonesia"
+sub = "indonesia+indonesiacirclejerk"
 
 def mars_lyric():
 	lyric = """
-		MARILAH SELURUH RAKYAT INDONESIA
-		ARAHKAN PANDANGANMU KE DEPAN
-		RAIHLAH MIMPIMU BAGI NUSA BANGSA
-		SATUKAN TEKADMU TUK MASA DEPAN
-		PANTANG.... MENYERAH.... 
-		ITULAH ... PEDOMANMU
-		ENTASLAH KEMISKINAN CITA-CITAMU
-		RINTANGAN TAK MENGGETARKAN DIRIMU
-		INDONESIA MAJU, SEJAHTERA, TUJUANMU
-		NYALAKAN API SEMANGAT PERJUANGAN
-		DENGUNGKAN GEMA, NYATAKAN PERSATUAN
-		OLEH PERINDO....OLEH PERINDO.... 
-		JAYALAH INDONESIA
+**M**ARILAH SELURUH RAKYAT INDONESIA
+
+**A**RAHKAN PANDANGANMU KE DEPAN
+
+**R**AIHLAH MIMPIMU BAGI NUSA BANGSA
+
+**S**ATUKAN TEKADMU TUK MASA DEPAN
+		
+&nbsp;
+
+**P**ANTANG... MENYERAH... ITULAH... PEDOMANMU
+
+**E**NTASLAH KEMISKINAN CITA-CITAMU
+
+**R**INTANGAN TAK MENGGETARKAN DIRIMU
+
+**I**NDONESIA MAJU, SEJAHTERA, TUJUANMU
+
+**N**YALAKAN API SEMANGAT PERJUANGAN
+
+**D**ENGUNGKAN GEMA, NYATAKAN PERSATUAN
+
+**O**LEH PERINDO... OLEH PERINDO.... 
+
+&nbsp;
+
+**JAYALAH INDONESIA**
 		"""
 	return lyric
 
@@ -29,14 +44,14 @@ def bot_login():
 
 def main(reddit):
 	for comment in reddit.subreddit(sub).stream.comments():
-		if ("perindo" in comment.body.lower() or 
-			"mars" in comment.body.lower()):
-				name = comment.author.name
-				if (bot != name):
-					print("comment found! by " + name)
-					print(comment.body)
-					print("replying...")
-					comment.reply(mars_lyric())
+		if ("mars perindo" in comment.body.lower()):
+			name = comment.author.name
+			if (bot != name):
+				print("comment found! by " + name)
+				print(">> " + comment.body)
+				print("replying...")
+				comment.reply(mars_lyric())
+				time.sleep(60)
 
 while True:
 	reddit = bot_login()
